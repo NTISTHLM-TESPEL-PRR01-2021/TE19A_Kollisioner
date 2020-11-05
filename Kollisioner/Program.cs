1c34a949c6e6e5d2c564b0893d751be1589d26f6
Mikael <mikael.bergstrom@ga.ntig.se>
2020-11-05 14:11:27 +0100

    Work done

--- Kollisioner/Program.cs
+++ Kollisioner/Program.cs
@@ -1,12 +1,40 @@
 ﻿using System;
+using Raylib_cs;
 
 namespace Kollisioner
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+
+      Raylib.InitWindow(800, 600, "Kollisoner");
+
+      Rectangle playerRect = new Rectangle(0, 0, 100, 50);
+      Rectangle enemyRect = new Rectangle(50, 60, 30, 30);
+
+      Color red = new Color(255, 0, 0, 128);
+
+      while (!Raylib.WindowShouldClose())
+      {
+        //playerRect.x += 0.1f;
+        playerRect.y += 0.01f;
+
+        Raylib.BeginDrawing();
+
+        Raylib.ClearBackground(Color.LIGHTGRAY);
+
+        Raylib.DrawRectangleRec(playerRect, red);
+        Raylib.DrawRectangleRec(enemyRect, red);
+
+        if (Raylib.CheckCollisionRecs(playerRect, enemyRect))
         {
-            Console.WriteLine("Hello World!");
+          Raylib.DrawText("Colliding!", 0, 550, 32, Color.BLACK);
         }
+
+        Raylib.EndDrawing();
+      }
+
     }
+  }
 }
